bc8f1540b69b916e3efd9a5061f7d36a43065622
Chantel Punt <csky.punt@gmail.com>
2023-02-02 17:07:25 +1000

    advanced routing, middleware, parsing form/json data

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,54 @@
+const e = require('express')
 const express = require('express')
 const router = express.Router()
 
+router.use(logger)
+
 router.get('/', (req, res) => {
-    res.send('User List')
+    console.log(req.query.name)
+    res.send('Users List')
 })
 
 router.get('/new', (req, res) =>{
-    res.send('New User Form')
+    res.render('users/new')
 })
 
 router.post('/', (req, res) => {
-    res.send('Create User')
+    const isValid = true
+    if (isValid) {
+      users.push({firstName: req.body.firstName})
+      res.redirect(`/users/${users.length - 1}`)
+    }
+    else {
+      console.log('Error')
+      res.render('users/new', {firstName: req.body.firstName})
+    }
 })
 
-router.get('/:id', (req, res) => {
-    req.params.id
+router
+  .route('/:id')
+  .get((req, res) => {
+    console.log(req.user)
     res.send(`Get User with ID ${req.params.id}`)
+  })
+  .put((req, res) => {
+    req.params.id
+    res.send(`Update User with ID ${req.params.id}`)
+  })
+  .delete((req, res) => {
+    req.params.id
+    res.send(`Delete User with ID ${req.params.id}`)
+  })
+
+const users = [{name: 'Chantel'}, {name: 'Kyle'}]
+router.param('id', (req,res, next, id) => {
+  req.user = users[id]
+  next()
 })
+
+function logger(req, res, next) {
+  console.log(req.originalUrl)
+  next()
+}
 
 module.exports = router
